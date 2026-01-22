04a440c6132717a7a2685c4bae6a96dcc81e5631
Sparsh Bansal <115392431+bansalsparsh@users.noreply.github.com>
2026-01-22 16:02:00 +0530

    Add files via upload

---
+++ Projects/ReflectionWithDll/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try{
+            string path = @"C:\Users\HP\Desktop\C# .net\ReflectionTest\bin\Debug\net10.0\ReflectionTest.dll";
+            var a = Assembly.LoadFrom(path);
+
+            Console.WriteLine($"Classes present are:");
+            foreach(Type x in a.GetTypes().Where(t=>t.IsClass))
+            {
+                Console.WriteLine("\nClass name is: "+x.Name);
+                MethodInfo[] meth = x.GetMethods();
+                Console.WriteLine($"Methods present in class {x.Name} are:");
+                foreach(var m in meth)
+                {
+                    Console.WriteLine(m.Name);
+                }
+            }
+
+            var inter = a.GetTypes().Where(t=>t.IsInterface);
+            Console.WriteLine($"\nInterfaces present are:");
+            foreach(var x in inter)
+            {
+                Console.WriteLine("\nInterface name is: "+x.Name);
+                MethodInfo[] meth = x.GetMethods();
+                Console.WriteLine($"Methods present in Interfaces {x.Name} are:");
+                foreach(var m in meth)
+                {
+                    Console.WriteLine(m.Name);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
